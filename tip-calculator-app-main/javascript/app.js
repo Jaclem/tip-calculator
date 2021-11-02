50ad084e5fa0ded31a002bc4adff4f1d226db02e
Jaclem <jaclem12@gmail.com>
2021-11-01 23:01:41 -0400

    JS updated
    Javascript now outputs the correct values that I want, a little more tweaking and it will be all set.

--- tip-calculator-app-main/javascript/app.js
+++ tip-calculator-app-main/javascript/app.js
@@ -1,20 +1,30 @@
 
-let percentages = document.querySelectorAll('li a').addEventListener("click", includePercentage);
+// let percentages = document.getElementById('5').addEventListener("click", calculate);
+let percentages = document.querySelectorAll('li a');
 
-function includePercentage() {
-    let x = percentages.innerHTML;
-    console.log(x);
+
+ 
+// for loop creates a variable percentage that loops through and adds the event listener to
+// each a tag belonging to the variable percentages
+for (let percentage of percentages) {
+    percentage.addEventListener('click', detect, false);
+
+    function detect() {
+        return tipAmount = percentage.id / 100; // divides the id percentage value by 100 giving the decimal value needed to calculate
+    }
+
 }
 
 
 // Treats the reset button as a clickable value that will execute the calculate function when clicked.
 const reset = document.getElementById('reset').addEventListener("click", calculate);
 
-function calculate(){
-    // The variables that store the values need to be in this function to work.
+function calculate() {
     const billAmount = document.getElementById('bill').value;
     const customTip = document.getElementById('custom').value;
     const amountOfPeople = document.getElementById('number-of-people').value;
 
-    console.log(billAmount);
+    let tip = detect();
+    let tipPrice = billAmount * tip / amountOfPeople;
+    console.log(tipPrice.toFixed(2));
 }
